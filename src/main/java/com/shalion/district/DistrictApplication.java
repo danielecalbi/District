bfbcc9fc4c55d9f22383324d0f6c13855363c216
Daniele <daniele.calbi.pt@gmail.com>
2026-04-28 18:14:53 +0200

    Fix

--- src/main/java/com/shalion/district/DistrictApplication.java
+++ src/main/java/com/shalion/district/DistrictApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DistrictApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(DistrictApplication.class, args);
     }
 
